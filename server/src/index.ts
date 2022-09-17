dfa1f92b9b73305e1d1a73b36f9910beaaa80cee
Justin Xu <xu.justin.j@gmail.com>
2022-09-17 02:24:38 -0400

    Set up Express server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,3 +1,10 @@
-const PORT = process.env.PORT || 3001;
+import express from "express";
+import cors from "cors";
 
-console.log(`Once we implement a server, it will listen on port ${PORT}.`);
+const PORT = process.env.PORT || 3001;
+const app = express();
+app.use(cors());
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
