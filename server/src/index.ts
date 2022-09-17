8ce94b81e1b0303dc6a86469a0f118a027cceb26
Arnav Shah <sarn248@gmail.com>
2022-09-17 15:10:57 -0400

    Init ibm tts

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,9 @@ import * as dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
 import { CohereSummarizer } from "./summarizer";
+import TextToSpeechV1 from 'ibm-watson/text-to-speech/v1'
+import { IamAuthenticator } from 'ibm-watson/auth'
+import { tTS } from "./speechToText";
 
 dotenv.config();
 
@@ -16,6 +19,31 @@ const summarizer = new CohereSummarizer(COHERE_API_KEY);
 app.post("/summary", async (req, res) => {
   res.json(await summarizer.summarize(req.body.text));
 });
+
+const textToSpeech = new TextToSpeechV1({
+  authenticator: new IamAuthenticator({
+    apikey: process.env.IBM_KEY!,
+  }),
+  serviceUrl: 'https://api.us-east.text-to-speech.watson.cloud.ibm.com/instances/c23b276d-bd22-4bba-b646-0f2d20aaa607',
+});
+app.get("/voices", (req, res) => {
+  textToSpeech.listVoices()
+    .then(voices => {
+      res.send(JSON.stringify(voices, null, 2))
+    })
+    .catch(err => {
+      console.log('error:', err);
+    });
+});
+
+app.post("/tts", (req, res) => {
+  const { summary, model, folder } = req.body;
+  console.log(summary, model, folder);
+
+  tTS(summary, model, folder)
+  res.send("success");
+})
+
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
