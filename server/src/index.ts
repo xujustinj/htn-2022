87da4c5796dfb84aa553df7ec8aacb0822423942
Justin Xu <xu.justin.j@gmail.com>
2022-09-17 22:39:00 -0400

    Get reasonable article summarization working

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,11 @@
+import cors from "cors";
 import * as dotenv from "dotenv";
 import express from "express";
-import cors from "cors";
-import TextToSpeechV1 from 'ibm-watson/text-to-speech/v1'
-import { IamAuthenticator } from 'ibm-watson/auth'
+import TextToSpeechV1 from "ibm-watson/text-to-speech/v1";
+import { IamAuthenticator } from "ibm-watson/auth";
 import { WikipediaExtractor } from "./extractor";
-import { CohereSummarizer } from "./summarizer";
-import { tTS } from "./speechToText";
+import { CohereSummarizer } from "./summarize/summarizer";
+import { tts } from "./textToSpeech";
 
 dotenv.config();
 
@@ -15,25 +15,22 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const COHERE_API_KEY = process.env.COHERE_API_KEY!;
-const summarizer = new CohereSummarizer(COHERE_API_KEY);
-app.post("/summary", async (req, res) => {
-  res.json(await summarizer.summarize(req.body.text));
-});
-
 const textToSpeech = new TextToSpeechV1({
   authenticator: new IamAuthenticator({
     apikey: process.env.IBM_KEY!,
   }),
-  serviceUrl: 'https://api.us-east.text-to-speech.watson.cloud.ibm.com/instances/c23b276d-bd22-4bba-b646-0f2d20aaa607',
+  serviceUrl:
+    "https://api.us-east.text-to-speech.watson.cloud.ibm.com/instances/c23b276d-bd22-4bba-b646-0f2d20aaa607",
 });
 app.get("/voices", (req, res) => {
-  textToSpeech.listVoices()
-    .then(voices => {
-      res.send(JSON.stringify(voices, null, 2))
+  textToSpeech
+    .listVoices()
+    // @ts-ignore
+    .then((voices) => {
+      res.send(JSON.stringify(voices, null, 2));
     })
-    .catch(err => {
-      console.log('error:', err);
+    .catch((err: unknown) => {
+      console.log("error:", err);
     });
 });
 
@@ -41,11 +38,24 @@ app.post("/tts", (req, res) => {
   const { summary, model, folder } = req.body;
   console.log(summary, model, folder);
 
-  tTS(summary, model, folder)
+  tts(summary, model, folder);
   res.send("success");
-})
+});
 
+const COHERE_API_KEY = process.env.COHERE_API_KEY!;
+const summarizer = new CohereSummarizer(COHERE_API_KEY);
+const extractor = new WikipediaExtractor();
+app.post("/summary", async (req, res) => {
+  const { title } = req.body;
 
-app.listen(PORT, () => {
+  const content = await extractor.extract(title);
+  const sentences = await summarizer.summarizeSection(content);
+  res.json(sentences);
+
+  // coming soon
+  // sentences.forEach((s, i) => tts(s, "en-US_AllisonV3Voice", title));
+});
+
+app.listen(PORT, async () => {
   console.log(`Server listening on ${PORT}`);
 });
