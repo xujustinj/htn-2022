0540851e42a1ff4b7625132dada514bfc4d946dd
Justin Xu <xu.justin.j@gmail.com>
2022-09-18 03:11:42 -0400

    Generate visual keyphrases for summaries

--- server/src/cohere/client.ts
+++ server/src/cohere/client.ts
@@ -1,7 +1,11 @@
 import cohere from "cohere-ai";
 import { TextNode } from "../types/textTree";
 import { Sample } from "../types/sample";
-import { longSummarySamples, shortSummarySamples } from "./samples";
+import {
+  keyphraseSamples,
+  longSummarySamples,
+  shortSummarySamples,
+} from "./samples";
 
 const DELIMITER = "--";
 
@@ -22,7 +26,8 @@ export class CohereClient {
     preamble: string,
     signifier: string,
     trainingSamples: ReadonlyArray<Sample>,
-    job: Sample
+    job: Sample,
+    maxTokens: number
   ) {
     const prompt = [
       preamble,
@@ -32,8 +37,8 @@ export class CohereClient {
 
     return {
       model: "large",
-      prompt: prompt,
-      max_tokens: 256,
+      prompt: prompt.trim(),
+      max_tokens: maxTokens,
       stop_sequences: [DELIMITER],
       temperature: 0.3,
     };
@@ -43,9 +48,16 @@ export class CohereClient {
     preamble: string,
     signifier: string,
     trainingSamples: ReadonlyArray<Sample>,
-    job: Sample
+    job: Sample,
+    options: {
+      maxTokens?: number;
+      bounceLength?: number;
+    } = {}
   ) {
-    if (wordCount(job.query) < 32) {
+    const maxTokens = options.maxTokens ?? 128;
+    const bounceLength = options.bounceLength ?? 0;
+
+    if (wordCount(job.query) < bounceLength) {
       return job.query;
     }
 
@@ -53,12 +65,17 @@ export class CohereClient {
       preamble,
       signifier,
       trainingSamples,
-      job
+      job,
+      maxTokens
     );
-    console.log("Cohere request:", JSON.stringify(request, undefined, 2));
+    // console.log("Cohere request:", JSON.stringify(request, undefined, 2));
 
     const response = await cohere.generate(request);
-    console.log("Cohere response:", JSON.stringify(response, undefined, 2));
+    // console.log("Cohere response:", JSON.stringify(response, undefined, 2));
+
+    console.log(
+      `\n\n========\n\n${request.prompt}${response.body.generations[0].text}`
+    );
 
     return response.body.generations[0].text.split(DELIMITER)[0].trim();
   }
@@ -69,19 +86,31 @@ export class CohereClient {
     const signifier = "Summary:";
 
     if (node.children.length === 0) {
-      return this.cohere(preamble, signifier, shortSummarySamples, {
-        query: node.value,
-        response: "",
-      });
+      return this.cohere(
+        preamble,
+        signifier,
+        shortSummarySamples,
+        {
+          query: node.value,
+          response: "",
+        },
+        { bounceLength: 32 }
+      );
     } else {
       let summaries: Array<Promise<string>> = [];
 
       if (wordCount(node.value) > 0) {
         summaries.push(
-          this.cohere(preamble, signifier, shortSummarySamples, {
-            query: node.value,
-            response: "",
-          })
+          this.cohere(
+            preamble,
+            signifier,
+            shortSummarySamples,
+            {
+              query: node.value,
+              response: "",
+            },
+            { bounceLength: 32 }
+          )
         );
       }
 
@@ -89,21 +118,40 @@ export class CohereClient {
         ...node.children.map((child) => this.summarizeNode(child, quota))
       );
 
-      let summary = (await Promise.all(summaries)).join(" ");
+      const summary = (await Promise.all(summaries)).join(" ");
       let condensate = "";
       while (wordCount(condensate) < quota) {
-        condensate = await this.cohere(
-          preamble,
-          signifier,
-          longSummarySamples,
-          {
-            query: summary,
-            response: condensate,
-          }
-        );
+        condensate +=
+          " " +
+          (await this.cohere(
+            preamble,
+            signifier,
+            longSummarySamples,
+            {
+              query: summary,
+              response: condensate,
+            },
+            { bounceLength: 0 }
+          ));
       }
 
       return condensate;
     }
+  }
+
+  public async getKeyphrase(text: string): Promise<string> {
+    const preamble = "Generate a visual description of each phrase.";
+    const signifier = "Visual:";
+
+    return this.cohere(
+      preamble,
+      signifier,
+      keyphraseSamples,
+      {
+        query: text,
+        response: "",
+      },
+      { maxTokens: 8 }
+    );
   }
 }
