61a843cfbe04f7fa30b3e147199df7077096c8f2
Arnav Shah <sarn248@gmail.com>
2022-09-18 00:02:31 -0400

    Added More Pages

--- client/src/components/common/videoCreation/Voice.jsx
+++ client/src/components/common/videoCreation/Voice.jsx
@@ -30,45 +30,51 @@ export function Voice() {
                     <a href="/vid" class="inline-block p-4 rounded-t-lg hover:text-gray-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300">Length</a>
                 </li>
                 <li class="mr-2">
-                    <a href="/formality"  class="inline-block p-4 rounded-t-lg hover:text-gray-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300">Formality</a>
+                    <a href="/formality" class="inline-block p-4 rounded-t-lg hover:text-gray-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300">Formality</a>
                 </li>
                 <li class="mr-2">
                     <a href="/voice" aria-current="page" class="flex p-4 text-blue-600 bg-gray-100 rounded-t-lg active dark:bg-gray-800 dark:text-blue-500" >Voice</a>
                 </li>
                 <li class="mr-2">
-                    <a href="#" class="inline-block p-4 rounded-t-lg hover:text-gray-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300">Music</a>
+                    <a href="/music" class="inline-block p-4 rounded-t-lg hover:text-gray-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300">Music</a>
                 </li>
 
             </ul>
+            <div className='flex container mx-auto'>
 
-            <div id="info-popup" tabindex="-1" className=" flex justify-start mt-60 ml-6 overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal md:h-full">
-                <div class="relative p-4 w-full max-w-lg h-full md:h-auto">
-                    <div class="relative p-4 bg-white rounded-lg shadow dark:bg-gray-800 md:p-8">
-                        <div class="mb-4 text-sm font-light text-gray-500 dark:text-gray-400">
-                            <h3 class="mb-3 text-2xl font-bold text-gray-900 dark:text-white">Which AI Model would you like to speak?</h3>
+                <div id="info-popup" tabindex="-1" className=" flex justify-start mt-60 ml-6 overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal md:h-full">
+                    <div class="relative p-4 w-full max-w-lg h-full md:h-auto">
+                        <div class="relative p-4 bg-white rounded-lg shadow dark:bg-gray-800 md:p-8">
+                            <div class="mb-4 text-sm font-light text-gray-500 dark:text-gray-400">
+                                <h3 class="mb-3 text-2xl font-bold text-gray-900 dark:text-white">Which AI Model would you like to speak?</h3>
 
-                            <div class="justify-between items-center pt-0 space-y-4 sm:flex sm:space-y-0">
-            
-              <div class="items-center space-y-4 sm:space-x-4 sm:flex sm:space-y-0">
-                  <button id="close-modal" type="button"  class="py-2 px-4 w-full text-sm font-medium text-gray-500 bg-white rounded-lg border border-gray-200 sm:w-auto hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-primary-300 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">Cancel</button>
-                  <button id="confirm-button" type="button" class="py-2 px-4 w-full text-sm font-medium text-center text-white rounded-lg bg-primary-700 sm:w-auto hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Confirm</button>
-              </div>
-          </div>
+                                <div class="justify-between items-center pt-0 space-y-4 sm:flex sm:space-y-0">
+
+                                    <div class="items-center space-y-4 sm:space-x-4 sm:flex sm:space-y-0">
+                                        <button id="close-modal" type="button" class="py-2 px-4 w-full text-sm font-medium text-gray-500 bg-white rounded-lg border border-gray-200 sm:w-auto hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-primary-300 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">Male English</button>
+                                        <button id="confirm-button" type="button" class="py-2 px-4 w-full text-sm font-medium text-gray-500 bg-white rounded-lg border border-gray-200 sm:w-auto hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-primary-300 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">Female English</button>
+                                        <button id="confirm-button" type="button" class="py-2 px-4 w-full text-sm font-medium text-gray-500 bg-white rounded-lg border border-gray-200 sm:w-auto hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-primary-300 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">Male Spanish</button>
+                                    </div>
+                                </div>
+                                </div>
 
 
 
-                           
 
+
+
+
+                            </div>
 
                         </div>
-
                     </div>
+
+
                 </div>
+
 
 
             </div>
 
-
-        </div>
-    )
+            )
 }
